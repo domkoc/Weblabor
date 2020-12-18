8dded7a5a5cd64e8ee1f6865f6f014a129e47711
Dominik Kocka <kocka98@gmail.com>
2020-12-18 12:44:59 +0100

    Labor 13

---
+++ Labor 13 - Haladó JavaScript/Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+
+namespace GuessWS
+{
+    public class Player
+    {
+        public WebSocket Socket { get; set; }
+
+        public string Name { get; set; }
+
+        public int CurrentGuesses { get; set; }
+    }
+}
---
+++ Labor 13 - Haladó JavaScript/Models/ToplistItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+
+namespace GuessWS
+{
+    public class ToplistItem
+    {
+        public string Name { get; set; }
+        public int Guesses { get; set; }
+        public int TimeInSeconds { get; set; }
+    }
+}
